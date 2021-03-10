246ff7cde579756b969a17fbf00771091b16e826
Felix Koch <mail@felixkoch.de>
2021-03-10 16:12:30 +0100

    poc get data from apaleo

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,51 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+import { ClientCredentials } from 'simple-oauth2'
+
+
+export default async (req, res) => {
+  const config = {
+    client: {
+      id: process.env.APALEO_CLIENT_ID,
+      secret: process.env.APALEO_CLIENT_SECRET,
+      
+    },
+    auth: {
+      tokenHost: process.env.APALEO_IDENTITY_URL,
+      tokenPath: "/connect/token"
+    }
+  };
+
+  const client = new ClientCredentials(config);
+ 
+  const tokenParams = {
+    scope: 'reservations.read',
+  };
+ 
+  try {
+    const accessToken = await client.getToken(tokenParams, {json: true});
+    console.log(accessToken)
+    console.log(accessToken.token.access_token)
+
+    const response = await fetch('https://api.apaleo.com/booking/v1/reservations',{
+      headers: {
+        //'Content-Type': 'application/json',
+        'authorization': "Bearer "+accessToken.token.access_token,
+      },
+    })
+
+    const data = await response.json()
+
+    console.log(data)
+
+
+
+
+  } catch (error) {
+    console.log('Access Token error', error.message);
+  }
+
+
+
   res.status(200).json({ name: 'John Doe' })
 }
